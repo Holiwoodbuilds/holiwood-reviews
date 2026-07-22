289e961692442a4619caa57df74d67cd358c51dc
Holiwoodbuilds <info@holiwood.ca>
2026-07-22 17:32:18 -0400

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', padding: 40 }}>
+      <h1>Holiwood Reviews API</h1>
+      <p>This service powers the review widget on holiwood.ca.</p>
+      <p>
+        Go to <a href="/admin">/admin</a> to manage reviews.
+      </p>
+    </div>
+  );
+}
